b3f0fa5a6c6383dde4de8b89253856947efa1e8c
haythambouja97 <101941931+haythambouja97@users.noreply.github.com>
2022-05-05 16:33:21 +0100

    upload file

---
+++ Models/Repositories/IEmployeeRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApplication2.Models
+{
+    public interface IEmployeeRepository
+    {
+        Employe GetEmploye(int id);
+        IEnumerable<Employe> GetAllEmploye();
+        Employe Add(Employe employe);
+        Employe Update(Employe employe);
+        Employe Delete(int id);
+
+    }
+}
---
+++ Models/Repositories/SqlEmployeeRepository.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication2.Models
+{
+    public class SqlEmployeeRepository : IEmployeeRepository
+    {
+        private readonly AppDbContext context;
+
+        public SqlEmployeeRepository(AppDbContext context)
+        {
+            this.context = context;
+        }
+
+        public Employe Add(Employe employe)
+        {
+            context.Employees.Add(employe);
+            context.SaveChanges();
+            return employe;
+        }
+
+        public Employe Delete(int id)
+        {
+            Employe employe = context.Employees.Find(id);
+            if(employe != null)
+            {
+                context.Employees.Remove(employe);
+                context.SaveChanges();
+            }
+            return employe;
+        }
+
+        public IEnumerable<Employe> GetAllEmploye()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Employe GetEmploye(int id)
+        {
+            return context.Employees.Find(id);
+        }
+
+        public Employe Update(Employe employeeChanges)
+        {
+            var employee = context.Employees.Attach(employeeChanges);
+            employee.State = EntityState.Modified;
+            context.SaveChanges();
+            return employeeChanges;
+        }
+    }
+}
